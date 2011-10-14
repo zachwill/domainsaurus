285fa23acc0b227ec7aa72e389da7bf651eb4e6b
Zach Williams <hey@zachwill.com>
2011-10-14 00:03:50 -0700

    Updates to coffeescript and CSS

--- static/js/words.coffee
+++ static/js/words.coffee
@@ -1,48 +1,55 @@
-domainr = (word) ->
-  # Return the appropriate Domainr API URL.
-  "http://domai.nr/api/json/search?q=#{ word }"
+class Search
+  """A class to encapsulate search functionality."""
+
+  constructor: ->
+    @form = $('form')
+    @input = $('.search-bar')
+    @form.submit(this.perform_search)
+    @input.focus()
+
+  domainr: (word) ->
+    "http://domai.nr/api/json/search?q=#{ word }"
+
+  wordnik: (word, type='definitions') ->
+    api_key = '114c5c8013a30746b185b088e83026eaebebd4a243890747e'
+    "http://api.wordnik.com/v4/word.json/#{ word }/#{ type }?api_key=#{ api_key }"
+
+  perform_search: (event) =>
+    value = @input.val()
+    if @input.data('api') == 'wordnik'
+      url = this.wordnik(value)
+    else
+      url = this.domainr(value)
+    this.ajax_call(url)
+    return false
+
+  ajax_call: (url) =>
+    $.ajax({
+      url: url
+      dataType: 'jsonp'
+      success: this.populate
+    })
+
+  populate: (data) =>
+    if @input.data('api') == 'wordnik'
+      # Populate results based on Wordnik data.
+    else
+      # Populate results based on Domainr data.
+    console.log(data)
+
+  input_check: =>
+    value = @input.val()
+    if value.length >= 2
+      window._value = value
+      setTimeout(this.check_value, 50)
+
+  check_value: =>
+    value = @input.val()
+    if value == window._value
+      this.perform_search()
 
 
-wordnik = (word, type='definitions') ->
-  # Return the appropriate Wordnik API URL.
-  api_key = '114c5c8013a30746b185b088e83026eaebebd4a243890747e'
-  "http://api.wordnik.com/v4/word.json/#{ word }/#{ type }?api_key=#{ api_key }"
+main = ->
+  new Search
 
-
-search = (event) ->
-  # Fire event on word search -- for either Domainr or Wordnik.
-  input = $('input')
-  if input.data('wordnik')
-    url = wordnik(input.val())
-  else if input.data('domainr')
-    url = domainr(input.val())
-  $.ajax({
-    url: url
-    dataType: 'json'
-    success: populate
-  })
-
-
-populate = (data) ->
-  # Work with returned JSON data from API calls.
-  input = $('input')
-  if input.data('wordnik')
-    api = 'wordnik'
-  else if input.data('domainr')
-    api = 'domainr'
-  data
-
-
-input_check = ->
-  # If the input value is a certain length, go ahead and perform a search.
-  value = $('input').val()
-  if (value.length >= 2)
-    window._value = value
-    setTimeout(check_value, 50)
-
-
-check_value = ->
-  # A timeout check to see that the input value has stayed the same.
-  value = $('input').val()
-  if value == window._value
-    search
+main()
--- static/js/words.js
+++ static/js/words.js
@@ -1,53 +1,71 @@
 (function() {
-  var check_value, domainr, input_check, populate, search, wordnik;
-  domainr = function(word) {
-    return "http://domai.nr/api/json/search?q=" + word;
-  };
-  wordnik = function(word, type) {
-    var api_key;
-    if (type == null) {
-      type = 'definitions';
+  var Search, main;
+  var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; };
+  Search = (function() {
+    "A class to encapsulate search functionality.";    function Search() {
+      this.check_value = __bind(this.check_value, this);
+      this.input_check = __bind(this.input_check, this);
+      this.populate = __bind(this.populate, this);
+      this.ajax_call = __bind(this.ajax_call, this);
+      this.perform_search = __bind(this.perform_search, this);      this.form = $('form');
+      this.input = $('.search-bar');
+      this.form.submit(this.perform_search);
+      this.input.focus();
     }
-    api_key = '114c5c8013a30746b185b088e83026eaebebd4a243890747e';
-    return "http://api.wordnik.com/v4/word.json/" + word + "/" + type + "?api_key=" + api_key;
+    Search.prototype.domainr = function(word) {
+      return "http://domai.nr/api/json/search?q=" + word;
+    };
+    Search.prototype.wordnik = function(word, type) {
+      var api_key;
+      if (type == null) {
+        type = 'definitions';
+      }
+      api_key = '114c5c8013a30746b185b088e83026eaebebd4a243890747e';
+      return "http://api.wordnik.com/v4/word.json/" + word + "/" + type + "?api_key=" + api_key;
+    };
+    Search.prototype.perform_search = function(event) {
+      var url, value;
+      value = this.input.val();
+      if (this.input.data('api') === 'wordnik') {
+        url = this.wordnik(value);
+      } else {
+        url = this.domainr(value);
+      }
+      this.ajax_call(url);
+      return false;
+    };
+    Search.prototype.ajax_call = function(url) {
+      return $.ajax({
+        url: url,
+        dataType: 'jsonp',
+        success: this.populate
+      });
+    };
+    Search.prototype.populate = function(data) {
+      if (this.input.data('api') === 'wordnik') {} else {
+
+      }
+      return console.log(data);
+    };
+    Search.prototype.input_check = function() {
+      var value;
+      value = this.input.val();
+      if (value.length >= 2) {
+        window._value = value;
+        return setTimeout(this.check_value, 50);
+      }
+    };
+    Search.prototype.check_value = function() {
+      var value;
+      value = this.input.val();
+      if (value === window._value) {
+        return this.perform_search();
+      }
+    };
+    return Search;
+  })();
+  main = function() {
+    return new Search;
   };
-  search = function(event) {
-    var input, url;
-    input = $('input');
-    if (input.data('wordnik')) {
-      url = wordnik(input.val());
-    } else if (input.data('domainr')) {
-      url = domainr(input.val());
-    }
-    return $.ajax({
-      url: url,
-      dataType: 'json',
-      success: populate
-    });
-  };
-  populate = function(data) {
-    var api, input;
-    input = $('input');
-    if (input.data('wordnik')) {
-      api = 'wordnik';
-    } else if (input.data('domainr')) {
-      api = 'domainr';
-    }
-    return data;
-  };
-  input_check = function() {
-    var value;
-    value = $('input').val();
-    if (value.length >= 2) {
-      window._value = value;
-      return setTimeout(check_value, 50);
-    }
-  };
-  check_value = function() {
-    var value;
-    value = $('input').val();
-    if (value === window._value) {
-      return search;
-    }
-  };
+  main();
 }).call(this);
